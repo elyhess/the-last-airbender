9d9ad456e6b29aeca88892f39d39a220f9de3454
Ely Hess <Ely.hess@me.com>
2021-02-12 09:49:55 -0700

    Add character poro

---
+++ app/poros/character.rb
@@ -0,0 +1,15 @@
+class Character
+	attr_reader :name,
+	            :photo,
+	            :allies,
+	            :enemies,
+	            :affiliation
+
+	def initialize(data)
+		@name = data[:name]
+		@image = data[:photoUrl]
+		@allies = data[:allies].join(", ")
+		@enemies = data[:enemies].join(", ")
+		@affiliation = data[:affiliation]
+	end
+end
