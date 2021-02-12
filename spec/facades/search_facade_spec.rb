84cf8e731efbe0e1a7b680c9633fdd40ecb71598
Ely Hess <Ely.hess@me.com>
2021-02-12 09:49:22 -0700

    Add refactor specs

---
+++ spec/facades/search_facade_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+describe SearchFacade do
+	describe "::characters_by_nation" do
+		it "returns character data" do
+			search = SearchFacade.characters_by_nation("fire+nation")
+			expect(search).to be_a(Array)
+			expect(search.first).to be_a(Character)
+			expect(search.count).to eq(25)
+		end
+	end
+end
---
+++ spec/poros/character_spec.rb
@@ -0,0 +1,28 @@
+require "rails_helper"
+
+RSpec.describe Character do
+	it "exists" do
+		character = Character.new({
+			                          name:        "some_name",
+			                          allies:      ["ali1", "ali2"],
+			                          enemies:     ["ene1", "ene2"],
+			                          affiliation: 'some affiliation'
+		                          })
+
+		expect(character).to be_an_instance_of(Character)
+	end
+
+	it 'has attributes' do
+		character = Character.new({
+			                          name:        "some_name",
+			                          allies:      ["ali1", "ali2"],
+			                          enemies:     ["ene1", "ene2"],
+			                          affiliation: 'some affiliation'
+		                          })
+
+		expect(character.name).to eq("some_name")
+		expect(character.allies).to eq("ali1, ali2")
+		expect(character.enemies).to eq("ene1, ene2")
+		expect(character.affiliation).to eq("some affiliation")
+	end
+end
