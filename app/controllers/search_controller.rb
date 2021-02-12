ef894884d8b17596bc4608d35d6dd60e8d7adefc
Ely Hess <Ely.hess@me.com>
2021-02-12 09:49:41 -0700

    Add search refactor

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,10 +1,6 @@
 class SearchController < ApplicationController
 	def index
 		nation = params[:nation].gsub('_', '+')
-
-		conn = Faraday.new("https://last-airbender-api.herokuapp.com")
-		response = conn.get("/api/v1/characters?affiliation=#{nation}&perPage=25")
-
-		@characters = JSON.parse(response.body, symbolize_names: true)
+		@characters = SearchFacade.characters_by_nation(nation)
 	end
 end
